589062a0ebf1f785ff66d60ef428ecc6f8f03ae4
mwasyl <mwasyl@gmail.com>
2015-11-02 15:50:24 +0100

    Initial Step, template for Kata excercise

---
+++ StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculatorKata.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+    }
+}
---
+++ StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,7 @@
+﻿
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+    }
+}
